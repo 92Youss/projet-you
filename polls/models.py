0d8bd7794522a90b8fc940f7c928aa65f36591f1
youssoupha <yusufa.ndiaay@gmail.com>
2024-03-28 17:06:54 +0100

    update my model

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class MyClass(models.Model):
+class MyNodel(models.Model):
     property1 = models.CharField(max_length=100)  # Define property1 as a CharField
     property2 = models.IntegerField()  # Define property2 as an IntegerField
-
-    def my_method(self, arg1, arg2):
-        # Method code here
-        return arg1 + arg2
 
     def another_method(self):
         # Method code here
